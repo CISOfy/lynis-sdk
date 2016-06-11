fc5542698361db0f9a69f90e6241860bed239e41
Michael Boelen <michael.boelen@cisofy.com>
2016-06-11 14:40:22 +0200

    Renumbering of tests

--- unit-tests/tests-functions.sh
+++ unit-tests/tests-functions.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 RETVAL=0
+TOTAL=2
 
 Assert() {
 
-    printf "[1/1] DirectoryExists (valid directory) "
+    printf "[1/${TOTAL}] DirectoryExists (valid directory) "
     if DirectoryExists .; then printf "OK\n"; else printf "FAILED\n"; RETVAL=1; fi
-    printf "[1/2] DirectoryExists (invalid directory) "
+    printf "[2/${TOTAL}] DirectoryExists (invalid directory) "
     if ! DirectoryExists non-existing-dir; then printf "OK\n"; else printf "FAILED\n"; RETVAL=1; fi
 
     return ${RETVAL}
